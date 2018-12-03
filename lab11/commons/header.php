a62e43a35a2896f19762eb05e855a1154c63a05b
root <luann4099@gmail.com>
2018-12-03 21:00:32 +0700

    - fix bugs at header.php file - implement login.php

--- lab11/commons/header.php
+++ lab11/commons/header.php
@@ -1,8 +1,6 @@
 <head>
-    <meta charset="utf-8">
-    <title>Admin</title>
-    <link rel="stylesheet"
-    href="../statics/bootstrap/css/bootstrap.min.css"/>
-    <script type="text/javascript"
-     src="../statics/bootstrap/js/bootstrap.min.js"/>
+      <meta charset="utf-8">
+      <title>Admin</title>
+      <link href="../lab11/statics/bootstrap/css/bootstrap.min.css"/>
+      <script src="../lab11/statics/bootstrap/js/bootstrap.min.js"> </script>
 </head>
---
+++ lab11/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <?php require('commons/header.php'); ?>
+  <body>
+    <form>
+      <div class="form-group">
+        <input type="text" class="form-control" name="username" placeholder="username">
+      </div>
+
+      <div class="form-group">
+        <input type="text" class="form-control" name="pwd" placeholder="password">
+      </div>
+
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+
+    <?php require('commons/footer.php'); ?>
+  </body>
+</html>
