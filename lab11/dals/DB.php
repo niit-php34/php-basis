1ea972fc96978575782fbe0a339b253ab9095c83
root <luann4099@gmail.com>
2018-12-03 19:52:49 +0700

    - init DB class

---
+++ lab11/dals/DB.php
@@ -0,0 +1,33 @@
+<?php
+   class DB
+   {
+     var $pdo;
+     var $host;
+     var $dbname;
+     var $username;
+     var $pass;
+     function __construct(
+     $host='localhost',
+     $dbname='news',
+     $username='root',
+     $pass='koodinh')
+     {
+       $this->host = $host;
+       $this->dbname=$dbname;
+       $this->username=$username;
+       $this->pass=$pass;
+     }
+
+     function connect(){
+       $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->$dbname
+       ,$this->username
+       ,$this->pass);
+       return $this->pdo;
+     }
+
+     function disconnect(){
+       $this->pdo=null;
+       return true;
+     }
+   }
+?>
