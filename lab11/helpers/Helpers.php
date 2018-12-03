a6909e871c5fe363bafb1c5ae50f61cce3a113b5
root <luann4099@gmail.com>
2018-12-03 20:35:03 +0700

    - init helpers

---
+++ lab11/helpers/Helpers.php
@@ -0,0 +1,20 @@
+<?php
+  class Helpers
+  {
+    function __construct()
+    {
+
+    }
+
+    function encryptPwd($pwd){
+      return md5(md5($pwd));
+    }
+
+    function checkLogin(){
+      if(!isset($_SESSION['user']) || $_SESSION['user']==null){
+        return false;
+      }
+      return true;
+    }
+  }
+ ?>
